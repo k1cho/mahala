da39fa0cf47c9cd03885ebcbe1a3c4390cdddd38
k1cho <aadnanhadzipasic@gmail.com>
2019-02-14 20:52:47 +0100

    automatic login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenService } from './services/token.service';
+import { routerNgProbeToken } from '@angular/router/src/router_module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mahala';
+
+  constructor(private tokenService: TokenService, private router: Router) {}
+  ngOnInit() {
+    const token: string = this.tokenService.getToken();
+
+    if (token) {
+      this.router.navigate(['streams']);
+    } else {
+      this.router.navigate(['/']);
+    }
+  }
 }
