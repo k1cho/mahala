1b39e00e730cd6e1bcace113b26c2d99cbbb51ca
k1cho <aadnanhadzipasic@gmail.com>
2019-02-15 19:21:50 +0100

    navbar and styles

--- src/app/modules/streams.module.ts
+++ src/app/modules/streams.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StreamsComponent } from '../components/streams/streams.component';
 import { TokenService } from '../services/token.service';
+import { ToolbarComponent } from '../components/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [StreamsComponent],
+  declarations: [StreamsComponent, ToolbarComponent],
   imports: [CommonModule],
-  exports: [StreamsComponent],
+  exports: [StreamsComponent, ToolbarComponent],
   providers: [TokenService]
 })
 export class StreamsModule {}
