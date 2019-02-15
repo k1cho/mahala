61db4689e33f6913eb388f764f1a7501073ceb5e
k1cho <aadnanhadzipasic@gmail.com>
2019-02-15 20:25:31 +0100

    posts form and styles

--- src/app/modules/streams.module.ts
+++ src/app/modules/streams.module.ts
@@ -4,9 +4,11 @@ import { StreamsComponent } from '../components/streams/streams.component';
 import { TokenService } from '../services/token.service';
 import { ToolbarComponent } from '../components/toolbar/toolbar.component';
 import { SideBarComponent } from '../components/side-bar/side-bar.component';
+import { PostFormComponent } from '../components/posts/post-form/post-form.component';
+import { PostsComponent } from '../components/posts/posts/posts.component';
 
 @NgModule({
-  declarations: [StreamsComponent, ToolbarComponent, SideBarComponent],
+  declarations: [StreamsComponent, ToolbarComponent, SideBarComponent, PostFormComponent, PostsComponent],
   imports: [CommonModule],
   exports: [StreamsComponent, ToolbarComponent],
   providers: [TokenService]
