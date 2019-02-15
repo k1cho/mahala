ac945acd332464ed8b3450867ce31f4e1946e4db
k1cho <aadnanhadzipasic@gmail.com>
2019-02-15 19:58:38 +0100

    sidebar and styles

--- src/app/modules/streams.module.ts
+++ src/app/modules/streams.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { StreamsComponent } from '../components/streams/streams.component';
 import { TokenService } from '../services/token.service';
 import { ToolbarComponent } from '../components/toolbar/toolbar.component';
+import { SideBarComponent } from '../components/side-bar/side-bar.component';
 
 @NgModule({
-  declarations: [StreamsComponent, ToolbarComponent],
+  declarations: [StreamsComponent, ToolbarComponent, SideBarComponent],
   imports: [CommonModule],
   exports: [StreamsComponent, ToolbarComponent],
   providers: [TokenService]
