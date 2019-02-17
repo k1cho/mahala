07b66839a3d68ddcd190de45007deb9bcc895334
k1cho <aadnanhadzipasic@gmail.com>
2019-02-17 20:14:54 +0100

    comment list html and css

--- src/app/components/comments/comment-list/comment-list.component.ts
+++ src/app/components/comments/comment-list/comment-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-comment-list',
   templateUrl: './comment-list.component.html',
   styleUrls: ['./comment-list.component.css']
 })
-export class CommentListComponent implements OnInit {
-
-  constructor() { }
+export class CommentListComponent implements OnInit, AfterViewInit {
+  toolbarElement: any;
+  formData: any;
+  constructor() {}
 
   ngOnInit() {
+    this.toolbarElement = document.querySelector('.nav-content');
+    this.formData = {};
   }
 
+  ngAfterViewInit() {
+    this.toolbarElement.style.display = 'none';
+  }
+
+  createComment() {
+    console.log(this.formData);
+  }
 }
