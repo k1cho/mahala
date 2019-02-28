78e4a572b5a16f4ec41f8d9991886fccd5ea5277
k1cho <aadnanhadzipasic@gmail.com>
2019-02-28 14:45:44 +0100

    display notifications

--- src/app/components/notifications-list/notifications-list.component.ts
+++ src/app/components/notifications-list/notifications-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from 'src/app/services/token.service';
+import { UsersService } from 'src/app/services/users.service';
+import { Socket } from 'ngx-socket-io';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-notifications-list',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications-list.component.css']
 })
 export class NotificationsListComponent implements OnInit {
+  loggedUser: any;
+  notifications: [];
 
-  constructor() { }
+  constructor(private tokenService: TokenService, private usersSerivce: UsersService, private socket: Socket) {}
 
   ngOnInit() {
+    this.loggedUser = this.tokenService.getPayload();
+    this.getNotifications();
+    this.socket.on('refreshPage', () => {
+      this.getNotifications();
+    });
   }
 
+  getNotifications() {
+    this.usersSerivce.getUserById(this.loggedUser._id).subscribe(user => {
+      this.notifications = user.notifications;
+    });
+  }
+
+  timeFromNow(time) {
+    return moment(time).fromNow();
+  }
 }
