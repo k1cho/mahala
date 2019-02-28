aab5276dfc493e660bf265f5a0705e1ac58b3561
k1cho <aadnanhadzipasic@gmail.com>
2019-02-28 19:34:40 +0100

    display notifications from dropdown

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { TokenService } from 'src/app/services/token.service';
+import * as M from 'materialize-css';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -9,11 +11,29 @@ import { TokenService } from 'src/app/services/token.service';
 })
 export class ToolbarComponent implements OnInit {
   user: any;
+  notifications: [];
 
-  constructor(private authService: AuthService, private tokenService: TokenService) {}
+  constructor(
+    private authService: AuthService,
+    private tokenService: TokenService,
+    private usersService: UsersService
+  ) {}
 
   ngOnInit() {
     this.user = this.tokenService.getPayload();
+    const dropdown = document.querySelector('.dropdown-trigger');
+    M.Dropdown.init(dropdown, {
+      alignment: 'right',
+      coverTrigger: false
+    });
+
+    this.getNotifications();
+  }
+
+  getNotifications() {
+    this.usersService.getUserById(this.user._id).subscribe(user => {
+      this.notifications = user.notifications.reverse();
+    });
   }
 
   logout() {
