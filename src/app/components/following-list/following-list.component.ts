40ccf56bc7349090d52275c95e2176f1fefae3c5
k1cho <aadnanhadzipasic@gmail.com>
2019-02-28 13:27:11 +0100

    display followed user

--- src/app/components/following-list/following-list.component.ts
+++ src/app/components/following-list/following-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from 'src/app/services/token.service';
+import { UsersService } from 'src/app/services/users.service';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-following-list',
@@ -6,11 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./following-list.component.css']
 })
 export class FollowingListComponent implements OnInit {
-  constructor() {}
+  users: [];
+  loggedUser: any;
 
-  ngOnInit() {}
+  constructor(private tokenService: TokenService, private usersService: UsersService, private socket: Socket) {}
+
+  ngOnInit() {
+    this.loggedUser = this.tokenService.getPayload();
+    this.getUsers();
+    this.socket.on('refreshPage', () => {
+      this.getUsers();
+    });
+  }
+
+  getUsers() {
+    this.usersService.show(this.loggedUser._id).subscribe(user => {
+      this.users = user.following;
+    });
+  }
 
   unfollow(id) {
-    console.log(id);
+    this.usersService.unfollow(id).subscribe(
+      data => {
+        this.socket.emit('refresh', {});
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 }
