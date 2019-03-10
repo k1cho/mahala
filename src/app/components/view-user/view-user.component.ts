c8c8ea8b83f9acaac051bbf43f0be96f42db4253
k1cho <aadnanhadzipasic@gmail.com>
2019-03-10 23:20:59 +0100

    show posts, following and followers on profile

--- src/app/components/view-user/view-user.component.ts
+++ src/app/components/view-user/view-user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import M from 'materialize-css';
+import { ActivatedRoute } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-view-user',
@@ -12,15 +14,37 @@ export class ViewUserComponent implements OnInit, AfterViewInit {
   followingTab = false;
   followersTab = false;
 
-  constructor() {}
+  posts: [];
+  following: [];
+  followers: [];
+  user: any;
+  username: any;
+
+  constructor(private route: ActivatedRoute, private usersService: UsersService) {}
 
   ngOnInit() {
     this.tablElement = document.querySelector('.nav-content');
     M.Tabs.init(document.querySelector('.tabs'), {});
+    this.route.params.subscribe(param => {
+      this.username = param.username;
+      this.getUser(this.username);
+    });
   }
 
   ngAfterViewInit() {
     this.tablElement.style.display = 'none';
+  }
+
+  getUser(username) {
+    this.usersService.getUserByUsername(username).subscribe(
+      user => {
+        this.user = user;
+        this.posts = user.posts;
+        this.followers = user.followers;
+        this.following = user.following;
+      },
+      err => console.log(err)
+    );
   }
 
   changeTab(value) {
