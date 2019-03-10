007dc45b52a1bad8210349efcd8fd854c26fcb32
k1cho <aadnanhadzipasic@gmail.com>
2019-03-10 21:44:09 +0100

    user profile styles and html

--- src/app/components/view-user/view-user.component.ts
+++ src/app/components/view-user/view-user.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import M from 'materialize-css';
 
 @Component({
   selector: 'app-view-user',
   templateUrl: './view-user.component.html',
   styleUrls: ['./view-user.component.css']
 })
-export class ViewUserComponent implements OnInit {
+export class ViewUserComponent implements OnInit, AfterViewInit {
+  tablElement: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.tablElement = document.querySelector('.nav-content');
+    M.Tabs.init(document.querySelector('.tabs'), {});
   }
 
+  ngAfterViewInit() {
+    this.tablElement.style.display = 'none';
+  }
 }
