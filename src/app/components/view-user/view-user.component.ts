ece01c1ba9e7dc4938c5e8cf51402af76bbdc387
k1cho <aadnanhadzipasic@gmail.com>
2019-03-11 13:37:41 +0100

    view logged user profile

--- src/app/components/view-user/view-user.component.ts
+++ src/app/components/view-user/view-user.component.ts
@@ -42,6 +42,7 @@ export class ViewUserComponent implements OnInit, AfterViewInit {
         this.posts = user.posts;
         this.followers = user.followers;
         this.following = user.following;
+        console.log(user.posts);
       },
       err => console.log(err)
     );
